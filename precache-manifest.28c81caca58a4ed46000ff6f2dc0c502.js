cdbeb02f1ad38e23e0ed0c3992a76ee3306e39a4
Shannon Sagehorn <srsagehorn@gmail.com>
2020-10-13 13:27:35 -0500

    Updates

---
+++ precache-manifest.28c81caca58a4ed46000ff6f2dc0c502.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74c21fff329dbf9c1a92d29b848c0bea",
+    "url": "/srsagehorn/reactPortfolioindex.html"
+  },
+  {
+    "revision": "46c97dbd65557b394abc",
+    "url": "/srsagehorn/reactPortfoliostatic/css/main.d9b35234.chunk.css"
+  },
+  {
+    "revision": "98016456bab3024d0315",
+    "url": "/srsagehorn/reactPortfoliostatic/js/2.c96c2eeb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/srsagehorn/reactPortfoliostatic/js/2.c96c2eeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46c97dbd65557b394abc",
+    "url": "/srsagehorn/reactPortfoliostatic/js/main.56c3cf27.chunk.js"
+  },
+  {
+    "revision": "1876739676bfcca83269",
+    "url": "/srsagehorn/reactPortfoliostatic/js/runtime-main.f38824cd.js"
+  }
+]);
